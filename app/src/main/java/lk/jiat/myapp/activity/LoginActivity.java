c8615420ab42a2900bbaa52eccdb1720be9c2449
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-17 16:16:08 +0530

    login_activity

--- app/src/main/java/lk/jiat/myapp/activity/LoginActivity.java
+++ app/src/main/java/lk/jiat/myapp/activity/LoginActivity.java
@@ -11,7 +11,7 @@ import lk.jiat.myapp.R;
 public class LoginActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
     }
 }
